d448f06f5cee47e28c9c61f4aa1131e29b1ec3be
Jacek Migdal <jacek@migdal.pl>
2014-12-26 18:42:27 -0800

    Port fixes

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,5 @@
-export DISPLAY=:655
-Xvfb :655 -shmem -screen 0 1366x768x16 &
-x11vnc -display :655 -N -forever &
+export DISPLAY=:123
+Xvfb :123 -shmem -screen 0 1366x768x16 &
+x11vnc -display :123 -passwd empty -rfbport 5555 -N -forever &
 
 su - selenium -c "start-selenium"
