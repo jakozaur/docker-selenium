64c677d8242b01173472f6c6e7bf157894d6d890
Jacek Migdal <jacek@migdal.pl>
2014-12-29 11:12:24 -0800

    Add optional scale

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+
 export DISPLAY=:123
-Xvfb :123 -shmem -screen 0 1366x768x16 &
-x11vnc -display :123 -passwd empty -rfbport 5555 -N -forever &
+export SCALE=${1:-1.0}
+
+Xvfb $DISPLAY -shmem -screen 0 1366x768x16 &
+sleep 0.5 # Make sure Xvfb started
+x11vnc -display $DISPLAY -scale $SCALE -passwd empty -rfbport 5555 -N -forever &
 
 su - selenium -c "start-selenium"
